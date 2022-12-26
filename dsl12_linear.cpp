d605d399cb92f8986a2c34afaf5495dd9dd13432
Shivam Singh <100526291+shivamsan@users.noreply.github.com>
2022-12-27 01:34:17 +0530

    Update dsl12_linear.cpp

--- dsl12_linear.cpp
+++ dsl12_linear.cpp
@@ -1,36 +1,45 @@
 #include<iostream>
 using namespace std;
 
-int main()
+int linear(int a[], int n, int key)
 {
-    int a[100],n, key ,i,flag=0;
-    
-    cout << "enter total number of SE students numbers "<<endl;
-    cin >> n;
-    
-    cout<<"Enter the roll number of SE students"<<endl;
-
-    for (i=0;i<n;i++)
+    int flag=0;
+    for (int i=0; i<n; i++)
     {
-        cin >> a[i];
-
-    }
-    cout <<"enter roll no of student to be search"<<endl;
-    cin>>key;
-
-    for(i=0;i<n;i++)
-    {
-        if (a[i]==key)
+        if (a[i] == key)
         {
-            cout<<"student found at position: "<<(i+1)<<endl;
-            flag =1;
-            break;
-        }
+        cout<<"Found at "<<(i+1)<<"th position"<<endl;
+        flag =1;
+        break;
+        
+        }   
     }
+    
     if(flag==0)
     {
-        cout<<"roll no not found"<<endl;
-
+        cout<<"student no found"<<endl;
     }
+
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter number of students : "<<endl;
+    cin>>n;
+    
+    int a[n];
+    cout<<"Enter roll num of students :"<<endl;
+    for (int i =0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    int key;
+    cout<<"Enter roll no of student to search : "<<endl;
+    cin>>key;
+
+    linear(a,n,key);
     return 0;
+
 }
