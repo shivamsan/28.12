673986fcf02b7816649865946d8e9fc3126c2118
Shivam Singh <100526291+shivamsan@users.noreply.github.com>
2022-12-26 23:39:35 +0530

    Update dsl10_que_job.cpp

--- dsl10_que_job.cpp
+++ dsl10_que_job.cpp
@@ -1,73 +1,93 @@
-#include <iostream>
-#define MAX 10
+#include<iostream>
 using namespace std;
+#define size 10
 
-struct queue
-{ int data[MAX];
-int front,rear;
-};
-
-class Queue
-{ struct queue q;
-public:
- Queue(){q.front=q.rear=-1;}
- int isempty();
-int isfull();
-void enqueue(int);
-int delqueue();
-void display();
-};
-int Queue::isempty()
+class  queue
 {
-return(q.front==q.rear)?1:0;
-}
-int Queue::isfull()
-{
-return(q.rear==MAX-1)?1:0;
-}
-void Queue::enqueue(int x)
-{ q.data[++q.rear]=x;}
-int Queue::delqueue()
-{ return q.data[++q.front];}
-void Queue::display()
-{ int i;
-cout<<"\n";
-for(i=q.front+1;i<=q.rear;i++)
-cout<<q.data[i]<<" ";
-}
+       
+    int start = -1;
+    int end = -1;
+    int array[size];
 
+    public:
+
+    void add_job(int x)
+    {
+        if (end ==size-1)
+        {
+            cout<<"queue is full"<<endl;
+            
+        }
+        end++;
+        array[end]=x;
+    }
+
+    int del_job()
+    {
+        if (start == end)
+        {
+            cout<<"queue is empty"<<endl;
+            return -1;
+        }
+        start++;
+        return array[start];
+    }
+
+    void display()
+    {
+        if (start == end)
+        {
+            cout<<"queue is empty"<<endl;
+        }
+        else
+        {
+            cout<<"queue contains: "<<endl;
+            for (int i = start+1; i <= end; i++)
+            {
+                cout<<array[i]<<" ";
+
+            }
+            
+        }
+        cout<<endl;
+
+        
+    }
+    
+};
 
 int main()
-{ Queue obj;
+{
+
+queue obj;
 int ch,x;
 do{
 cout<<"\n====MENU====\n 1.Insert Job\n 2.Delete Job\n 3.Display\n 4.Exit\n Enter your choice : "; 
 cin>>ch;
 switch(ch)
-{ case 1: if (!obj.isfull())
- { cout<<"\n Enter data : ";
-cin>>x;
-obj.enqueue(x);
-cout<<endl;
-}
- else
-cout<< "Queue is overflow!!!\n\n";
-break;
-case 2: if(!obj.isempty())
-cout<<"\n Deleted Element = "<<obj.delqueue()<<endl;
-else
-{ cout<<"\n Queue is underflow!!!\n\n"; }
-cout<<"\nRemaining Jobs : ";
-obj.display();
-break;
-case 3: if (!obj.isempty())
-{ cout<<"\n Queue contains : ";
-obj.display();
-}
-else
-cout<<"\n Queue is empty!!!\n\n";
-break;
-case 4: cout<<"\n Exiting Program.....";
+{ case 1:
+    cout<<"\n Enter data : ";
+    cin>>x;
+    obj.add_job(x);
+    cout<<endl;
+    break;
+case 2: 
+    cout<<"\n Deleted Element = "<<obj.del_job()<<endl;
+    obj.display();
+    break;
+case 3: 
+    cout<<"\n Queue contains : ";
+    obj.display();
+    break;
+
+
+case 4: 
+    cout<<"\n Exiting Program.....";
+    break;
+
+default:
+    cout<<"\n Invalid Input";
+    break;
 }
 }while(ch!=4);
 return 0;
